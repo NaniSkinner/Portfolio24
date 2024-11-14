a558f5cd84f569dcce8013fa55df3396648a6e3e
NaniSkinner <skinner.galvez@gmail.com>
2024-11-13 20:01:58 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { ThemeToggle } from "@/components/theme-toggle";
+
+export default function Home() {
+  return (
+    <main className="flex flex-1  w-full h-full gap-10 ">
+      {/* Left Side */}
+      <div className="flex-1 p-6 h-full max-w-md">
+        <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
+          Left Side
+        </div>
+      </div>
+      {/* Right Side */}
+      <div className="flex-1 p-6 h-full">
+        <div className="flex items-center justify-center h-full rounded-md dark:bg-emerald-700">
+          Right Side
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ config/site-config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  creator: "Nani Skinner",
+  title: "Technical Writer",
+  bio: "My husband made me do this.",
+  location: "earth 🌎",
+} as const;
